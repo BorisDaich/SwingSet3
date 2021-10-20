b13963d245b03f36a8cf64ca8f53b70eead83dd0
homebeaver <homebeaver@outlook.de>
2021-10-21 00:11:32 +0200

    show props when building

--- swingx-testsupport/src/test/java/org/jdesktop/test/EDTRunnerTest.java
+++ swingx-testsupport/src/test/java/org/jdesktop/test/EDTRunnerTest.java
@@ -2,6 +2,8 @@ package org.jdesktop.test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.logging.Logger;
+
 import javax.swing.SwingUtilities;
 
 import org.hamcrest.CoreMatchers;
@@ -10,11 +12,21 @@ import org.junit.runner.RunWith;
 
 @RunWith(EDTRunner.class)
 public class EDTRunnerTest {
+	
+    private static final Logger LOG = Logger.getLogger(EDTRunnerTest.class.getName());
+
     /**
      * Ensure that the EDTRunner is using the EventDispatchThread.
      */
     @Test
     public void testForSwingThread() {
+    	
+    	// Show props:
+    	LOG.info("java.version:"+System.getProperty("java.version"));
+    	System.getProperties().forEach((p,v) -> {
+    		LOG.info("Property: "+p+":"+v);
+    	});
+    	
         assertThat(SwingUtilities.isEventDispatchThread(), CoreMatchers.is(true));
     }
 }
