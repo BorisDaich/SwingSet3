f6eee408a1f19140b1915f76f4ea5a90ec9d89c5
homebeaver <homebeaver@outlook.de>
2021-10-23 16:06:57 +0200

    Nimbus class name

--- swingx-demos/swingx-demos-swingxset/src/main/java/org/jdesktop/swingxset/SwingXSet.java
+++ swingx-demos/swingx-demos-swingxset/src/main/java/org/jdesktop/swingxset/SwingXSet.java
@@ -28,7 +28,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.jdesktop.swingxset;
 
 import java.awt.BorderLayout;
@@ -139,8 +138,8 @@ public class SwingXSet extends SingleXFrameApplication  {
         UIManager.LookAndFeelInfo lafInfo[] = UIManager.getInstalledLookAndFeels();
         for(int i = 0; i < lafInfo.length; i++) {
             if (lafInfo[i].getName().equals("Nimbus")) {
-                lafInfo[i] = new UIManager.LookAndFeelInfo("Nimbus",
-                        "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+                lafInfo[i] = new UIManager.LookAndFeelInfo("Nimbus", "javax.swing.plaf.nimbus.NimbusLookAndFeel");
+// until Java 8: "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
                 break;
             }
         }
