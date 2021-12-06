36a3586ef53f59c53df1355cdfc8c521244a8ea5
homebeaver <homebeaver@outlook.de>
2021-12-06 13:57:39 +0100

    JXLoginPane Demo : locale lang selector + custom moon banner
    https://github.com/homebeaver/SwingSet/issues/16

--- swingx-demos/swingx-demos-content/src/main/java/org/jdesktop/swingx/demos/loginpane/LoginPaneDemo.java
+++ swingx-demos/swingx-demos-content/src/main/java/org/jdesktop/swingx/demos/loginpane/LoginPaneDemo.java
@@ -20,31 +20,36 @@ package org.jdesktop.swingx.demos.loginpane;
 
 import static org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ;
 
-import java.awt.AlphaComposite;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 import org.jdesktop.application.Application;
 import org.jdesktop.beansbinding.BeanProperty;
 import org.jdesktop.beansbinding.Bindings;
+import org.jdesktop.swingx.JXLabel;
 import org.jdesktop.swingx.JXLoginPane;
 import org.jdesktop.swingx.JXLoginPane.Status;
 import org.jdesktop.swingx.VerticalLayout;
+import org.jdesktop.swingx.demos.painter.PainterDemo;
 import org.jdesktop.swingx.plaf.basic.BasicLoginPaneUI;
-import org.jdesktop.swingx.util.GraphicsUtilities;
 
 import com.sun.swingset3.DemoProperties;
 
@@ -53,6 +58,7 @@ import com.sun.swingset3.DemoProperties;
  *
  * @author Karl George Schaefer
  * @author rah003 (original JXLoginPaneDemo)
+ * @author hb https://github.com/homebeaver (locale lang selector + custom moon banner)
  */
 @DemoProperties(
     value = "JXLoginPane Demo",
@@ -75,6 +81,7 @@ public class LoginPaneDemo extends JPanel {
     private JXLoginPane loginPane;
     private JButton loginLauncher;
     private JToggleButton allowLogin;
+    private JComboBox<Locale> localeBox; // TODO  JXComboBox is not generic
     
     /**
      * main method allows us to run as a standalone demo.
@@ -113,9 +120,9 @@ public class LoginPaneDemo extends JPanel {
         servers.add("B");
         loginPane.setServers(servers);
         
-//        loginPane.setBanner(null); // No banner (customizition)
-//        loginPane.setBanner(new DummyLoginPaneUI(loginPane).getBanner());
-        loginPane.setLocale(Locale.ITALIAN);
+        // customization:
+//        loginPane.setBanner(null); // No banner (customization)
+//        loginPane.setBanner(new MoonLoginPaneUI(loginPane).getBanner());
 //        loginPane.setBannerText("BannerText");
         
         loginLauncher = new JButton();
@@ -128,48 +135,69 @@ public class LoginPaneDemo extends JPanel {
         allowLogin = new JToggleButton();
         allowLogin.setName("allowLogin");
         p.add(allowLogin);
+        
+        p.add(new JXLabel("select language for Login Screen:", SwingConstants.CENTER));
+        localeBox = new JComboBox<Locale>();
+        localeBox.setModel(createLocaleList());
+        localeBox.addActionListener(event -> {
+        	Locale selected = (Locale)localeBox.getSelectedItem();
+        	loginPane.setLocale(selected);
+        });
+        p.add(localeBox);
+    }
+    
+    private ComboBoxModel<Locale> createLocaleList() {
+        DefaultComboBoxModel<Locale> model = new DefaultComboBoxModel<Locale>();
+//    private ComboBoxModel<E> createFilterList() {
+//        DefaultComboBoxModel<E> model = new DefaultComboBoxModel<E>();
+        model.addElement(Locale.ENGLISH);
+        model.addElement(new Locale("cs"));
+        model.addElement(new Locale("es"));
+        model.addElement(Locale.FRENCH);
+        model.addElement(Locale.GERMAN);
+        model.addElement(Locale.ITALIAN);
+        model.addElement(new Locale("nl"));
+        model.addElement(new Locale("pl"));
+        model.addElement(new Locale("pt", "BR"));
+        model.addElement(new Locale("sv"));
+		return model;
     }
     
     private void bind() {
     	loginLauncher.addActionListener(event -> {
-//    		Status status = JXLoginPane.showLoginDialog(LoginPaneDemo.this, loginPane); // returns status
-//    		LOG.info("status:"+status);
+    		Status status = JXLoginPane.showLoginDialog(LoginPaneDemo.this, loginPane); // returns status
+    		LOG.info("status:"+status);
     		// or per Frame:
-    		JFrame frame = JXLoginPane.showLoginFrame(loginPane);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//            frame.setJMenuBar(createAndFillMenuBar(panel));
-//            loginPane.setSaveMode(SaveMode.BOTH);
-            frame.pack();
-            frame.setVisible(true);
+//    		JFrame frame = JXLoginPane.showLoginFrame(loginPane);
+//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+////            frame.setJMenuBar(createAndFillMenuBar(panel));
+////            loginPane.setSaveMode(SaveMode.BOTH);
+//            frame.pack();
+//            frame.setVisible(true);
     	});
         Bindings.createAutoBinding(READ,
                 allowLogin, BeanProperty.create("selected"),
                 service, BeanProperty.create("validLogin")).bind();
     }
     
-    public class DummyLoginPaneUI extends BasicLoginPaneUI {
+    public class MoonLoginPaneUI extends BasicLoginPaneUI {
 
-        public DummyLoginPaneUI(JXLoginPane dlg) {
+        public MoonLoginPaneUI(JXLoginPane dlg) {
             super(dlg);
         }
 
         /**
-         * the original (super) default 400x60 banner is been resized to a (0, 0, 200, 100)-rectangle
+         * the original (super) default 400x60 banner is replaced by part of the moon
          */
         @Override
         public Image getBanner() {
-            Image banner = super.getBanner();
-            BufferedImage im = GraphicsUtilities.createCompatibleTranslucentImage(banner.getWidth(null), banner.getHeight(null));
-            Graphics2D g2 = im.createGraphics();
-            
-            try {
-                g2.setComposite(AlphaComposite.Src);
-                g2.drawImage(banner, 0, 0, 200, 100, null);
-            } finally {
-                g2.dispose();
-            }
-            
-            return im;
+        	try {
+        		BufferedImage im = ImageIO.read(PainterDemo.class.getResourceAsStream("moon.jpg"));
+        		return im.getSubimage(100, 300, 400, 60);
+        	} catch (IOException e) {
+        		LOG.warning("cannot read resource moon.jpg");
+        	}
+        	return super.getBanner();
         }
     }
 
