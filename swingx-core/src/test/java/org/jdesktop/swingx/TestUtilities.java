81b13ca7dcbdff9e81591689f2bb70d2985c91e6
homebeaver <homebeaver@outlook.de>
2022-01-08 19:26:46 +0100

    repair MatteBorderExtUnitTest.main

--- swingx-core/src/test/java/org/jdesktop/swingx/TestUtilities.java
+++ swingx-core/src/test/java/org/jdesktop/swingx/TestUtilities.java
@@ -1,10 +1,15 @@
 /*
- * $Id$
- *
  * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
  * Santa Clara, California 95054, U.S.A. All rights reserved.
  */
 package org.jdesktop.swingx;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JComponent;
 import javax.swing.UIManager;
@@ -25,10 +30,11 @@ import org.jdesktop.swingx.plaf.windows.WindowsLookAndFeelAddons;
  */
 public class TestUtilities {
 
+	private static final Logger LOG = Logger.getLogger(TestUtilities.class.getName());
+
   /**
    * Go through all existing LookAndFeelAddons. This leads all registered
-   * {@link org.jdesktop.swingx.plaf.ComponentAddon} to initialize/uninitialize
-   * themselves.
+   * {@link org.jdesktop.swingx.plaf.ComponentAddon} to initialize/uninitialize themselves.
    */
   public static void cycleAddons(JComponent component) throws Exception {
     LookAndFeelAddons.setAddon(MacOSXLookAndFeelAddons.class.getName());
@@ -55,26 +61,88 @@ public class TestUtilities {
 
     String property = UIManager.getString("win.xpstyle.name");
     try {
-      UIManager.put("win.xpstyle.name",
-        WindowsLookAndFeelAddons.HOMESTEAD_VISUAL_STYLE);
-      LookAndFeelAddons.setAddon(WindowsClassicLookAndFeelAddons.class
-        .getName());
+      UIManager.put("win.xpstyle.name", WindowsLookAndFeelAddons.HOMESTEAD_VISUAL_STYLE);
+      LookAndFeelAddons.setAddon(WindowsClassicLookAndFeelAddons.class.getName());
       component.updateUI();
 
       UIManager.put("win.xpstyle.name",
         WindowsLookAndFeelAddons.SILVER_VISUAL_STYLE);
-      LookAndFeelAddons.setAddon(WindowsClassicLookAndFeelAddons.class
-        .getName());
+      LookAndFeelAddons.setAddon(WindowsClassicLookAndFeelAddons.class.getName());
       component.updateUI();
 
       UIManager.put("win.xpstyle.name", null);
-      LookAndFeelAddons.setAddon(WindowsClassicLookAndFeelAddons.class
-        .getName());
+      LookAndFeelAddons.setAddon(WindowsClassicLookAndFeelAddons.class.getName());
       component.updateUI();
 
     } finally {
       UIManager.put("win.xpstyle.name", property);
     }
   }
+
+	public static InputStream getResourceAsStream(Class<?> clazz, String resourceName) {
+		
+		if(clazz==null) return getFileAsStream(resourceName);
+		
+		InputStream is = clazz.getResourceAsStream(resourceName); // Throws NullPointerException
+		LOG.finer("InputStream is:"+is);
+		if(is==null) {
+			LOG.log(Level.WARNING, "cannot find resource "+clazz.getName() + '#' + resourceName + " try FileAsStream ...");
+			// try FileInputStream:
+			
+			// first PackageName as dir
+			String dir = clazz.getPackageName().replace('.', '/')+'/';
+			is = getFileAsStream(dir, resourceName);
+			if(is!=null) return is;
+			
+			// this package org.jdesktop.swingx as dir
+			dir = TestUtilities.class.getPackageName().replace('.', '/')+'/';
+			is = getFileAsStream(dir, resourceName);
+			if(is!=null) return is;
+			
+			// try default eclipse test folder ...
+			String src = "src/test/resources/";
+			is = getFileAsStream(src+dir, resourceName);
+			if(is!=null) return is;
+
+			if(is==null) return getFileAsStream(resourceName);
+		}
+		return is;
+	}
+
+	public static InputStream getFileAsStream(String dir, String resourceName) {
+		File path = new File(dir);
+		if (!path.exists()) {
+			LOG.fine("(package)/path not found:"+path);
+			return null;
+		}
+		return getFileAsStream(dir+resourceName);
+	}
+
+	/**
+	 * try to load a resource from file with log info
+	 * 
+	 * @param resourceName fileName
+	 * @return (File)InputStream
+	 * 
+	 * @throws  NullPointerException
+     *          If the {@code resourceName} argument is {@code null}
+	 */
+	public static InputStream getFileAsStream(String resourceName) {
+        FileInputStream fis = null;
+		try {
+	        File file = new File(resourceName); // Throws NullPointerException - If the pathname argument is null
+	        if (!file.exists()) {
+	        	LOG.log(Level.WARNING, "cannot find resource "+file);
+	        } else {
+				LOG.info("found:"+file);
+	        	fis = new FileInputStream(file);
+	        }
+		} catch (NullPointerException e) {
+			throw e;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return fis;
+	}
 
 }
--- swingx-core/src/test/java/org/jdesktop/swingx/border/MatteBorderExtUnitTest.java
+++ swingx-core/src/test/java/org/jdesktop/swingx/border/MatteBorderExtUnitTest.java
@@ -1,14 +1,13 @@
 /*
- * $Id$
- *
  * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
  * Santa Clara, California 95054, U.S.A. All rights reserved.
  */
-
 package org.jdesktop.swingx.border;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -16,16 +15,19 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.jdesktop.swingx.TestUtilities;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-
 @RunWith(JUnit4.class)
 public class MatteBorderExtUnitTest extends junit.framework.TestCase {
-    public MatteBorderExtUnitTest() {
-	super("MatteBorderExt unit test");
-    }
+	
+//	private static final Logger LOG = Logger.getLogger(MatteBorderExtUnitTest.class.getName());
+
+	public MatteBorderExtUnitTest() {
+		super("MatteBorderExt unit test");
+	}
 
     // XXX placeholder
     @Test
@@ -61,55 +63,63 @@ public class MatteBorderExtUnitTest extends junit.framework.TestCase {
      * For unit testing only
      * @return test cases to be run in sequence
      */
-    private static TestCase[] createTestCases() {
-        final TestCase[] testCases = new TestCase[] {
+	private static TestCase[] createTestCases() {
+		final TestCase[] testCases = new TestCase[] {
 
-            new TestCase() {	// 1
-                @Override
-                public JComponent define() {
-                    Class<?>	klass = MatteBorderExtUnitTest.class;
-					Icon[]	icons = new Icon[] {
-                        new ImageIcon(klass.getResource("resources/images/wellTopLeft.gif")),
-                        new ImageIcon(klass.getResource("resources/images/wellTop.gif")),
-                        new ImageIcon(klass.getResource("resources/images/wellTopRight.gif")),
-                        new ImageIcon(klass.getResource("resources/images/wellRight.gif")),
-                        new ImageIcon(klass.getResource("resources/images/wellBottomRight.gif")),
-                        new ImageIcon(klass.getResource("resources/images/wellBottom.gif")),
-                        new ImageIcon(klass.getResource("resources/images/wellBottomLeft.gif")),
-                        new ImageIcon(klass.getResource("resources/images/wellLeft.gif")),
-                    };
+			new TestCase() { // 1
+				@Override
+				public JComponent define() {
+					Class<?> klass = MatteBorderExtUnitTest.class;
+					ArrayList<Icon> iconList = new ArrayList<Icon>();
+					String dir = "resources/images/";
+					try {
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellTopLeft.gif").readAllBytes()));
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellTop.gif").readAllBytes()));
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellTopRight.gif").readAllBytes()));
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellRight.gif").readAllBytes()));
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellBottomRight.gif").readAllBytes()));
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellBottom.gif").readAllBytes()));
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellBottomLeft.gif").readAllBytes()));
+						iconList.add(new ImageIcon(TestUtilities.getResourceAsStream(klass, dir+"wellLeft.gif").readAllBytes()));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 
-                    JPanel panel = new JPanel();
-                    panel.setBackground(Color.white);
-                    panel.setBorder(new MatteBorderExt(14, 14, 14, 14, icons));
-                    return panel;
-                }
-            },
+					JPanel panel = new JPanel();
+					panel.setBackground(Color.white);
+					Icon[] icons = new Icon[iconList.size()];
+					icons = iconList.toArray(icons);
+					panel.setBorder(new MatteBorderExt(14, 14, 14, 14, icons));
+					return panel;
+				}
+			},
 
-        };
-        return testCases;
-    }
+		};
+		return testCases;
+	}
 
-    private static abstract class TestCase {
+	private static abstract class TestCase {
 
-        public TestCase() {
-            this.frame = wrap(define());
-        }
+		public TestCase() {
+			this.frame = wrap(define());
+		}
 
-        public abstract JComponent define();
+		public abstract JComponent define();
 
-        public JFrame wrap(JComponent component) {
-            this.component = component;
-            final JFrame frame = new JFrame();
-            frame.getContentPane().add(component);
-            frame.pack();
-            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            return frame;
-        }
+		public JFrame wrap(JComponent component) {
+			this.component = component;
+			final JFrame frame = new JFrame();
+			frame.getContentPane().add(component);
+			frame.pack();
+			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			return frame;
+		}
 
-        @SuppressWarnings("unused")
-        public JComponent component;
-        public final JFrame frame;
+		@SuppressWarnings("unused")
+		public JComponent component;
+		public final JFrame frame;
 
-    }
+	}
+
 }
