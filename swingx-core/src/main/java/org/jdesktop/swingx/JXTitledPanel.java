8aeaecd6a9b7a867b68a72ec874d37f1640b6f98
homebeaver <homebeaver@outlook.de>
2022-06-16 15:35:01 +0200

    comment

--- swingx-core/src/main/java/org/jdesktop/swingx/JXTitledPanel.java
+++ swingx-core/src/main/java/org/jdesktop/swingx/JXTitledPanel.java
@@ -42,9 +42,9 @@ import org.jdesktop.swingx.plaf.TitledPanelUI;
  * <li>JXTitledPanel.titleBackground</li>
  * <li>JXTitledPanel.titleFont</li>
  * <li>JXTitledPanel.titlePainter</li>
- * <li>JXTitledPanel.captionInsets</li>
- * <li>JXTitledPanel.rightDecorationInsets</li>
- * <li>JXTitledPanel.leftDecorationInsets</li>
+ * <li>JXTitledPanel.captionInsets  ??? </li>
+ * <li>JXTitledPanel.rightDecoration</li>
+ * <li>JXTitledPanel.leftDecoration</li>
  * </ul>
  * 
  * @author Richard Bair
