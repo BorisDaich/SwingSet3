0474706d05fae3fb0b5aad8ad5738f02d503df1c
homebeaver <homebeaver@outlook.de>
2022-11-15 15:19:26 +0100

    interface RadianceIcon extends JXIcon näher an das Original von Kirill

--- swingx-core/src/main/java/org/jdesktop/swingx/icon/RadianceIcon.java
+++ swingx-core/src/main/java/org/jdesktop/swingx/icon/RadianceIcon.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2021 Radiance Kirill Grouchnikov. All Rights Reserved.
+ * Copyright (c) 2005-2022 Radiance Kirill Grouchnikov. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
@@ -40,60 +40,10 @@ import org.jdesktop.swingx.image.RadianceCC;
 /**
  * Extension of the core {@link Icon} interface that adds more capabilities like
  * resizing and color filtering.
- * <p>
- * SizingConstants extends SwingConstants with sizing from XS to XXL
  * 
  * @author Kirill Grouchnikov
- * @author EUG https://github.com/homebeaver (rotation + point/axis reflection)
  */
-public interface RadianceIcon extends Icon, SizingConstants {
-	
-	/**
-	 * A hint for point/axis reflection (mirroring) the icon when painting.
-	 * <p>
-	 * <code>setReflection(true, true)</code> means point reflection
-	 * 
-	 * @param horizontal will mirror the icon horizontal (X axis)
-	 * @param vertical will mirror the icon vertical (Y axis)
-	 * 
-	 */
-	// a default is necessary for icons generated before this feature was active
-	default void setReflection(boolean horizontal, boolean vertical) {}
-	default void setReflection(boolean pointReflection) {
-		setReflection(pointReflection, pointReflection);
-	}
-	default boolean isReflection() {
-		return false;
-	}
-	
-	/**
-	 * A hint to rotate the icon when painting
-	 * 
-	 * @param theta the angle of rotation in radians, zero means no rotation
-	 */
-	// a default is necessary for icons generated before this feature was active
-	default void setRotation(double theta) {}
-	default double getRotation() {
-		return 0d; // no rotation
-	}
-	/**
-	 * A hint to rotate the icon to a direction.
-	 * <p> The icon is aligned to {@code NORTH} per default, 
-	 * so rotate direction {@code NORTH_EAST} means rotating 45° right
-	 * and {@code WEST} means rotating 90° left or 270° right.
-	 * 
-	 * @param direction Compass-direction, use {@code SwingConstants} {@code NORTH}, {@code NORTH_EAST} etc
-	 * 
-	 * @see #setRotation(double)
-	 */
-	default void setRotation(int direction) {
-        if(direction>=NORTH && direction<=NORTH_WEST) {
-        	this.setRotation(Math.toRadians(45d*(direction-1)));
-        } else {
-            setRotation(0d); // no rotation for invalid directions
-        }
-	}
-
+public interface RadianceIcon extends JXIcon {
 	/**
 	 * Changes the dimension of <code>this</code> icon.
 	 * 
