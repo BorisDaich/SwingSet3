3b8dcc6b2ba80f1d79d5e13b3b7a58738e5ce65a
homebeaver <homebeaver@outlook.de>
2022-12-17 11:51:20 +0100

    use BorderFactory + prop "List.cellNoFocusBorder"

--- swingx-core/src/main/java/org/jdesktop/swingx/plaf/YListAddon.java
+++ swingx-core/src/main/java/org/jdesktop/swingx/plaf/YListAddon.java
@@ -1,6 +1,7 @@
 package org.jdesktop.swingx.plaf;
 
 import java.awt.Color;
+import java.util.logging.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.UIManager;
@@ -11,6 +12,8 @@ import javax.swing.plaf.metal.MetalLookAndFeel;
 import org.jdesktop.swingx.JYList;
 
 public class YListAddon extends AbstractComponentAddon {
+
+    private static final Logger LOG = Logger.getLogger(YListAddon.class.getName());
 
 	public YListAddon() {
 		super("JYList");
@@ -25,15 +28,18 @@ public class YListAddon extends AbstractComponentAddon {
          * replace the value with secondary3 Color
          */
         UIManager.getLookAndFeelDefaults().put("List.background", MetalLookAndFeel.getCurrentTheme().getControl());
+
         Border border = BorderFactory.createMatteBorder(1, 5, 1, 1, Color.red);
         UIManager.getLookAndFeelDefaults().put("List.focusSelectedCellHighlightBorder", border);
-        UIManager.getLookAndFeelDefaults().put("List.focusCellHighlightBorder", new BevelBorder(BevelBorder.RAISED));
+        UIManager.getLookAndFeelDefaults().put("List.focusCellHighlightBorder", BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        UIManager.getLookAndFeelDefaults().put("List.cellNoFocusBorder", BorderFactory.createEtchedBorder());
         
     }
 
     @Override
 	protected void addNimbusDefaults(LookAndFeelAddons addon, DefaultsList defaults) {
 		defaults.add(JYList.uiClassID, "org.jdesktop.swingx.plaf.synth.SynthYListUI");
+		LOG.info("added key "+JYList.uiClassID);
 
 		/*
 		 * key "List.background" is defined in javax.swing.plaf.nimbus.NimbusDefaults
@@ -41,7 +47,11 @@ public class YListAddon extends AbstractComponentAddon {
 		 * replace the value with "control" Color
 		 */
 		UIManager.getLookAndFeelDefaults().put("List.background", UIManager.getColor("control"));
-//		UIManager.getLookAndFeelDefaults().put("List.background", new ColorUIResource(214, 217, 223));
+
+		Border border = BorderFactory.createMatteBorder(1, 5, 1, 1, Color.red);
+        UIManager.getLookAndFeelDefaults().put("List.focusSelectedCellHighlightBorder", border);       
+        UIManager.getLookAndFeelDefaults().put("List.focusCellHighlightBorder", BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        UIManager.getLookAndFeelDefaults().put("List.cellNoFocusBorder", BorderFactory.createEtchedBorder());
 
 	}
 
