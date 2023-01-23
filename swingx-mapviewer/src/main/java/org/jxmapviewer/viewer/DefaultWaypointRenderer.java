ce02119a9300d8e26ba381045968d5254d5444fe
homebeaver <homebeaver@outlook.de>
2023-01-23 20:35:04 +0100

    DefaultWaypointRenderer : use Icon instead of BufferedImage

--- swingx-mapviewer/src/main/java/org/jxmapviewer/viewer/DefaultWaypointRenderer.java
+++ swingx-mapviewer/src/main/java/org/jxmapviewer/viewer/DefaultWaypointRenderer.java
@@ -14,42 +14,72 @@ import java.awt.image.BufferedImage;
 import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
 
 import org.jxmapviewer.JXMapViewer;
 
 /**
  * This is a standard waypoint renderer.
  * @author joshy
+ * @author EUG https://github.com/homebeaver (use Icon instead of BufferedImage)
  */
-public class DefaultWaypointRenderer implements WaypointRenderer<Waypoint>
-{
+public class DefaultWaypointRenderer implements WaypointRenderer<Waypoint> {
+	
 	private static final Logger LOG = Logger.getLogger(DefaultWaypointRenderer.class.getName());
     
     private BufferedImage img = null;
+    private Icon iconImg = null;
+//    private Object iconImg = null;
 
+    /**
+     * 
+     * @param wpIcon Waypoint icon to use
+     */
+    public DefaultWaypointRenderer(Icon wpIcon) {
+    	iconImg = wpIcon;
+    }
+    /**
+     * 
+     * @param wpImage Waypoint Image to use
+     */
+    public DefaultWaypointRenderer(BufferedImage wpImage) {
+    	if(wpImage==null) {
+    		try {
+    			img = ImageIO.read(DefaultWaypointRenderer.class.getResource("images/standard_waypoint.png"));
+    		} catch (Exception ex) {
+    			// this should not happen:
+            	LOG.warning("couldn't read standard_waypoint.png "+ ex);
+    		}
+    	} else {
+    		img = wpImage;
+    	}
+    }
     /**
      * Uses a default waypoint image
      */
-    public DefaultWaypointRenderer()
-    {
-        try
-        {
-            img = ImageIO.read(DefaultWaypointRenderer.class.getResource("images/standard_waypoint.png"));
-        }
-        catch (Exception ex)
-        {
-        	LOG.warning("couldn't read standard_waypoint.png "+ ex);
-        }
+    public DefaultWaypointRenderer() {
+    	this((BufferedImage)null);
     }
 
     @Override
-    public void paintWaypoint(Graphics2D g, JXMapViewer map, Waypoint w)
-    {
+    public void paintWaypoint(Graphics2D g, JXMapViewer map, Waypoint w) {
+    	if(iconImg!=null) {
+            Point2D point = map.getTileFactory().geoToPixel(w.getPosition(), map.getZoom());
+            
+            // unterstellt, dass die Spitze auf [img.getWidth() / 2 , img.getHeight()] ist:
+            int x = (int)point.getX() -iconImg.getIconWidth() / 2;
+            int y = (int)point.getY() -iconImg.getIconHeight();
+            
+            iconImg.paintIcon(map, g, x, y);
+            return;
+    	}
+    	
         if (img == null)
             return;
 
         Point2D point = map.getTileFactory().geoToPixel(w.getPosition(), map.getZoom());
         
+        // unterstellt, dass die Spitze auf [img.getWidth() / 2 , img.getHeight()] ist:
         int x = (int)point.getX() -img.getWidth() / 2;
         int y = (int)point.getY() -img.getHeight();
         
