6f3f89d2d0eb492cc7df6ada1f1e039a9ce6ae88
homebeaver <homebeaver@outlook.de>
2023-09-21 19:21:10 +0200

    combo box editor for colors
    * the background is painted with the color selected

--- swingx-core/src/test/java/org/jdesktop/swingx/JXComboBoxVisualCheck.java
+++ swingx-core/src/test/java/org/jdesktop/swingx/JXComboBoxVisualCheck.java
@@ -18,12 +18,18 @@
  */
 package org.jdesktop.swingx;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionListener;
 import java.util.logging.Logger;
 
 import javax.swing.ComboBoxEditor;
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.plaf.ColorUIResource;
 
 import org.jdesktop.swingx.decorator.HighlighterFactory;
 import org.jdesktop.swingx.renderer.DefaultListRenderer;
@@ -50,12 +56,15 @@ public class JXComboBoxVisualCheck extends InteractiveTestCase {
     
     public static void main(String[] args) {
 //        setSystemLF(true);
-    	setLAF("Nimb");
+//    	setLAF("Nimb");
         
         JXComboBoxVisualCheck test = new JXComboBoxVisualCheck();
         
 		try {
 			test.runInteractiveTests();
+//			test.runInteractiveTests("interactiveJXComboBoxEditing");
+//			test.runInteractiveTests("interactiveJXComboBoxColorEditing");
+			
 		} catch (Exception e) {
 			System.err.println("exception when executing interactive tests:");
 			e.printStackTrace();
@@ -100,6 +109,7 @@ public class JXComboBoxVisualCheck extends InteractiveTestCase {
     	{ "VERTICAL - a single column" 
     	, "VERTICAL_WRAP - flowing vertically then horizontally" 
     	, "HORIZONTAL_WRAP - flowing horizontally then vertically"
+    	, "your text"
     	};
     ComboBoxEditor ed;
     /**
@@ -108,13 +118,19 @@ public class JXComboBoxVisualCheck extends InteractiveTestCase {
      */
     public void interactiveJXComboBoxEditing() {        
     	JComboBox<String> cellsLayout = new JXComboBox<String>(LIST_LAYOUT_ORIENTATION);
-        JXFrame frame = wrapInFrame(cellsLayout, "shows editable JXComboBox with Strings");
+    	JPanel panel = new JPanel();
+    	panel.add(cellsLayout);
+        JXFrame frame = wrapInFrame(panel, "shows editable JXComboBox with Strings");
         cellsLayout.setSelectedIndex(1);
         cellsLayout.setName("cellsLayout");
     	cellsLayout.addActionListener(ae -> {
-        	cellsLayout.setSelectedIndex(cellsLayout.getSelectedIndex());
         	LOG.info("JComboBox ActionEvent "+ae);
-        	LOG.info("ComboBoxEditor "+ed + " the edited item:"+ ed.getItem());       	
+        	int i = cellsLayout.getSelectedIndex();
+        	if(i>-1) {
+            	cellsLayout.setSelectedIndex(i);
+            	LOG.info("ComboBoxEditor "+ed + " the edited item("+i+"):"+ ed.getItem());
+//        		cellsLayout.setEditable(i>=3); //LIST_LAYOUT_ORIENTATION.length-1);
+        	}
         });
     	cellsLayout.setEditable(true);
     	ed = cellsLayout.getEditor(); // not updeted when switch LaF
@@ -127,8 +143,8 @@ public class JXComboBoxVisualCheck extends InteractiveTestCase {
         	} else {
             	LOG.info("\n the selected item:"+ cellsLayout.getSelectedItem().getClass() +" "+cellsLayout.getSelectedItem()
             	+ "\n the edited item type:"+ ed.getItem().getClass() + " "+ed.getItem());
-            	int i = cellsLayout.getSelectedIndex();
-            	ComboBoxModel<String> m =cellsLayout.getModel();
+//            	int i = cellsLayout.getSelectedIndex();
+            	ComboBoxModel<String> m = cellsLayout.getModel();
 //            	// m implements interface MutableComboBoxModel:
 //            	if(m instanceof MutableComboBoxModel<String> mm) {
 //            		mm.removeElementAt(i);
@@ -143,6 +159,188 @@ public class JXComboBoxVisualCheck extends InteractiveTestCase {
             		String itemToadd = (String)ed.getItem();
             		dm.addElement(itemToadd);
             		dm.setSelectedItem(itemToadd);
+            	}
+        	}
+    	});
+        addComponentOrientationToggle(frame);
+        show(frame, 400, 300);
+    }
+    
+    public static Color[] colors = 
+    	{ Color.WHITE
+        , Color.YELLOW 
+    	, Color.BLACK
+    	// TODO colorcb.getSelectedIndex() bzw. colorcb.setSelectedItem findet das Objekt in Nimbus nicht
+    	// in Metal OK!
+    	, new ColorUIResource(102, 102, 153) // Metal primary1  
+    	, new Color(102, 102, 153)
+    	};
+    public class RedColorMsg extends Color {
+    	String m;
+    	public RedColorMsg(String msg) {
+    		super(Color.RED.getRGB());
+    		m = msg;
+    	}
+    	public String toString() {
+    		return m + " " + Color.class.getName() + "[r=" + getRed() + ",g=" + getGreen() + ",b=" + getBlue() + "]";
+    	}
+    }
+    // see public class BasicComboBoxEditor implements ComboBoxEditor,FocusListener {
+    public class ColorComboBoxEditor implements ComboBoxEditor {
+
+        protected JTextField editor;
+        private Object oldValue;
+
+        public ColorComboBoxEditor(Color color) {
+            editor = createEditorComponent(color);
+        }
+        protected JTextField createEditorComponent(Color color) {
+            JTextField editor = new JTextField(color.toString(),33);
+            editor.setBorder(null);
+            editor.setBackground(color);
+            return editor;
+        }
+
+		public Component setEditorComponent(Object o) {
+			if(o instanceof Color color) {
+				editor.setText(color.toString());
+	            editor.setBackground(color);
+			} else {
+				LOG.warning("??????????? Object not Color:"+o);
+			}
+			return editor;
+		}
+		
+		@Override
+		public Component getEditorComponent() {
+			return editor;
+		}
+
+		@Override
+		public void setItem(Object anObject) {
+			String text = "";
+			if(anObject instanceof Color color) {
+		        if ( anObject != null )  {
+		            text = anObject.toString();
+		            if (text == null) {
+		                text = "";
+		            }
+		            oldValue = anObject;
+//		            editor.setText(text);
+//		            editor.setBackground(color);
+		        }
+		        if (! text.equals(editor.getText())) { // kommt nicht dran
+		            editor.setText(text);
+		            editor.setBackground(color);
+		        }
+			} else {
+		        if (! text.equals(editor.getText())) {
+		            editor.setText(text);
+		        }
+			}
+		}
+
+		@Override
+		public Object getItem() {
+	        Object newText = editor.getText();
+	        Object newValue = editor.getBackground();
+	        if(!newValue.toString().endsWith(newText.toString())) {
+//	        if(!newText.equals(newValue)) {
+	        	newValue = newText;
+	        	LOG.warning("Object newValue:"+newValue);
+//	        	return new RedColorMsg(""+newText);
+	        }
+
+	        if (oldValue != null && !(oldValue instanceof Color))  {
+	            // The original value is not a color. Should return the value in it's original type.
+	            if (newValue.equals(oldValue.toString()))  {
+	                return oldValue;
+	            } else {
+	            	LOG.warning("??????????? Object oldValue:"+oldValue);
+	                // Must take the value from the editor and get the value and cast it to the new type.
+//	                Class<?> cls = oldValue.getClass();
+//	                try {
+//	                    Method method = MethodUtil.getMethod(cls, "valueOf", new Class<?>[]{String.class});
+//	                    newValue = MethodUtil.invoke(method, oldValue, new Object[] { editor.getText()});
+//	                } catch (Exception ex) {
+//	                    // Fail silently and return the newValue (a String object)
+//	                }
+	            }
+	        }
+	        return newValue;
+		}
+
+		@Override
+		public void selectAll() {
+	        editor.selectAll();
+	        editor.requestFocus();
+		}
+
+		@Override
+		public void addActionListener(ActionListener l) {
+	        editor.addActionListener(l);
+		}
+
+		@Override
+		public void removeActionListener(ActionListener l) {
+	        editor.removeActionListener(l);
+		}
+		
+//		// implement java.awt.event.FocusEvent
+//		@Override
+//		public void focusGained(FocusEvent e) {
+//			LOG.warning("??????????? FocusEvent:"+e);
+//		}
+//		// implement java.awt.event.FocusEvent
+//		@Override
+//		public void focusLost(FocusEvent e) {
+//			LOG.warning("??????????? FocusEvent:"+e);
+//		}
+    	
+    }
+    ComboBoxEditor colorEd; // = new ColorComboBoxEditor(Color.BLACK);
+    /**
+     * a combo box editor for colors (the background is painted with the color selected)
+     * you can change the edited item ==> then it is added to the combo box model
+     */
+    public void interactiveJXComboBoxColorEditing() {        
+    	JComboBox<Color> colorcb = new JXComboBox<Color>(colors);
+        JXFrame frame = wrapInFrame(colorcb, "shows editable JXComboBox with Colors");
+        colorcb.setSelectedIndex(3);
+        colorcb.setEditor(new ColorComboBoxEditor(colors[3]));
+        colorcb.setName("colorcb");
+    	colorcb.addActionListener(ae -> {
+        	LOG.info("JComboBox ActionEvent "+ae);
+//        	colorcb.setSelectedIndex(colorcb.getSelectedIndex());
+        	colorcb.setSelectedItem(colorcb.getSelectedItem());
+//        	((ColorComboBoxEditor)colorEd).setEditorComponent(colorcb.getSelectedItem());
+        	LOG.info("ComboBoxEditor "+colorEd + " the edited item:"+ colorEd.getItem());
+//        	colorEd.setItem(colorcb.getSelectedItem());
+        });
+    	colorcb.setEditable(true);
+    	colorEd = colorcb.getEditor(); // not updeted when switch LaF
+    	colorEd.addActionListener(ae -> {
+        	LOG.info("ComboBoxEditor ActionEvent "+ae);
+        	Object item = colorEd.getItem();
+        	LOG.info("??? ComboBoxEditor "+colorEd + " the edited item:"+ item);
+        	if(colorcb.getSelectedItem().equals(item)) {
+        		LOG.info("SelectedItem =============== edited Item");
+        		// nothing to do
+        	} else {
+            	LOG.info("\n the selected item:"+ colorcb.getSelectedItem().getClass() +" "+colorcb.getSelectedItem()
+            	+ "\n the edited item type:"+ item.getClass() + " "+item);
+            	int i = colorcb.getSelectedIndex();
+            	ComboBoxModel<Color> m = colorcb.getModel();
+            	if(m instanceof DefaultComboBoxModel<Color> dm) {
+            		if(item instanceof Color itemToadd ) {
+                		dm.addElement(itemToadd);
+                		dm.setSelectedItem(itemToadd);
+                	} else {
+                		LOG.warning("TODO item "+item);
+                		Color reditem = new RedColorMsg(""+item);
+                		dm.addElement(reditem);
+                		dm.setSelectedItem(reditem);
+            		}
             	}
         	}
     	});
