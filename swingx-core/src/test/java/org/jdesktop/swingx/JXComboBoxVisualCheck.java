77676da806508a606b3000bacdec611f23dfa526
homebeaver <homebeaver@outlook.de>
2023-09-26 10:17:21 +0200

    JXComboBoxVisualCheck interactiveJXComboBoxEditing EditorActionListener
    inner Class EditorActionListener statt lambda implementierung. Bei
    Lambda sommeln sich die listener, man kann sie nicht entfernen

--- swingx-core/src/test/java/org/jdesktop/swingx/JXComboBoxVisualCheck.java
+++ swingx-core/src/test/java/org/jdesktop/swingx/JXComboBoxVisualCheck.java
@@ -20,6 +20,7 @@ package org.jdesktop.swingx;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.logging.Logger;
 
@@ -105,63 +106,93 @@ public class JXComboBoxVisualCheck extends InteractiveTestCase {
         showInFrame(combo, "AlternateRowHighlighter - lineprinter");
     }
     
-    public static String[] LIST_LAYOUT_ORIENTATION = 
+    private final static String[] LIST_LAYOUT_ORIENTATION = 
     	{ "VERTICAL - a single column" 
     	, "VERTICAL_WRAP - flowing vertically then horizontally" 
     	, "HORIZONTAL_WRAP - flowing horizontally then vertically"
-    	, "your text"
     	};
+    JComboBox<String> cellsLayout;
     ComboBoxEditor ed;
+    EditorActionListener eal;
+    public class EditorActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent actionEvent) {
+//			LOG.info("editor actionEvent:"+actionEvent);
+			LOG.info("ComboBoxEditor "+ed + " the edited item:"+ ed.getItem());
+			if(cellsLayout.getSelectedItem().equals(ed.getItem())) {
+				LOG.info("SelectedItem =============== edited Item");
+			} else {
+				Object editedItem = ed.getItem();
+            	LOG.info("\n the selected item:"+ cellsLayout.getSelectedItem().getClass() +" "+cellsLayout.getSelectedItem()
+            	+ "\n the edited item type:"+ editedItem.getClass() + " "+editedItem);
+            	// add editedItem to model
+            	ComboBoxModel<String> m = cellsLayout.getModel();
+            	if(m instanceof DefaultComboBoxModel<String> dm) {
+                	dm.addElement((String)editedItem);
+                	dm.setSelectedItem(editedItem);
+            	}
+			}
+		}
+    	
+    }
     /**
      * logs the combo box editor
      * you can change the edited item ==> then it is added to the combo box model
+     * the last item is editable
      */
     public void interactiveJXComboBoxEditing() {        
-    	JComboBox<String> cellsLayout = new JXComboBox<String>(LIST_LAYOUT_ORIENTATION);
+    	cellsLayout = new JXComboBox<String>();
+    	for (int i=0,c=LIST_LAYOUT_ORIENTATION.length;i<c;i++ ) {
+    		cellsLayout.addItem(LIST_LAYOUT_ORIENTATION[i]);
+    	}
+    	cellsLayout.addItem("your editable text"); // this item is editable
     	JPanel panel = new JPanel();
     	panel.add(cellsLayout);
         JXFrame frame = wrapInFrame(panel, "shows editable JXComboBox with Strings");
         cellsLayout.setSelectedIndex(1);
         cellsLayout.setName("cellsLayout");
     	cellsLayout.addActionListener(ae -> {
-        	LOG.info("JComboBox ActionEvent "+ae);
+//        	LOG.info("JComboBox ActionEvent "+ae);
         	int i = cellsLayout.getSelectedIndex();
         	if(i>-1) {
             	cellsLayout.setSelectedIndex(i);
-            	LOG.info("ComboBoxEditor "+ed + " the edited item("+i+"):"+ ed.getItem());
-//        		cellsLayout.setEditable(i>=3); //LIST_LAYOUT_ORIENTATION.length-1);
-        	}
-        });
-    	cellsLayout.setEditable(true);
-    	ed = cellsLayout.getEditor(); // not updeted when switch LaF
-    	ed.addActionListener(ae -> {
-        	LOG.info("ComboBoxEditor ActionEvent "+ae);
-        	LOG.info("??? ComboBoxEditor "+ed + " the edited item:"+ ed.getItem());
-        	if(cellsLayout.getSelectedItem().equals(ed.getItem())) {
-        		LOG.info("SelectedItem =============== edited Item");
-        		// nothing to do
-        	} else {
-            	LOG.info("\n the selected item:"+ cellsLayout.getSelectedItem().getClass() +" "+cellsLayout.getSelectedItem()
-            	+ "\n the edited item type:"+ ed.getItem().getClass() + " "+ed.getItem());
-//            	int i = cellsLayout.getSelectedIndex();
-            	ComboBoxModel<String> m = cellsLayout.getModel();
-//            	// m implements interface MutableComboBoxModel:
-//            	if(m instanceof MutableComboBoxModel<String> mm) {
-//            		mm.removeElementAt(i);
-//            		mm.insertElementAt((String)ed.getItem(), i);
-//            	}
-            	if(m instanceof DefaultComboBoxModel<String> dm) {
-//            		//dm.removeElementAt(i);
-//            		dm.addElement((String)ed.getItem());
-////            		cellsLayout.setSelectedIndex(dm.getSize()); // IllegalArgumentException: setSelectedIndex: 4 out of bounds
-//            		dm.setSelectedItem(ed.getItem());
-//            		cellsLayout.updateUI(); // NPE
-            		String itemToadd = (String)ed.getItem();
-            		dm.addElement(itemToadd);
-            		dm.setSelectedItem(itemToadd);
+            	LOG.info("ComboBoxEditor "+ed + " , the edited item("+i+")");
+            	if(i>=LIST_LAYOUT_ORIENTATION.length) {
+            		cellsLayout.setEditable(true);
+            		ed = cellsLayout.getEditor();
+//            		LOG.info("# of editor ActionListeners "+ed.getActionListeners().length);
+            		//ed.removeActionListener(this);
+            		if(eal==null) {
+                		eal = new EditorActionListener();
+            		} else {
+            			ed.removeActionListener(eal);
+            		}
+            		ed.addActionListener(eal);
+            		// mit Lambda kann ich kein removeActionListener machen
+//            		ed.addActionListener(actionEvent -> {
+//            			LOG.info("editor actionEvent:"+actionEvent);
+//            			LOG.info("??? ComboBoxEditor "+ed + " the edited item:"+ ed.getItem());
+//            			if(cellsLayout.getSelectedItem().equals(ed.getItem())) {
+//            				LOG.info("SelectedItem =============== edited Item");
+//            			} else {
+//            				Object editedItem = ed.getItem();
+//                        	LOG.info("\n the selected item:"+ cellsLayout.getSelectedItem().getClass() +" "+cellsLayout.getSelectedItem()
+//                        	+ "\n the edited item type:"+ editedItem.getClass() + " "+editedItem);
+//                        	// add editedItem to model
+//                        	ComboBoxModel<String> m = cellsLayout.getModel();
+//                        	if(m instanceof DefaultComboBoxModel<String> dm) {
+//                            	dm.addElement((String)editedItem);
+//                            	dm.setSelectedItem(editedItem);
+//                        	}
+//            			}
+//            		});
+            	} else {
+            		cellsLayout.setEditable(false);
+            		ed = null;
             	}
         	}
-    	});
+        });
         addComponentOrientationToggle(frame);
         show(frame, 400, 300);
     }
