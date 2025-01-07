3aaca8e49247f7d246cd01cce0933f2b00bf8bea
homebeaver <homebeaver@outlook.de>
2025-01-07 14:45:43 +0100

    module swingx.core Schnittstelle LookAndFeelAddons

--- swingx-core/src/main/java/module-info.java
+++ swingx-core/src/main/java/module-info.java
@@ -1,11 +1,16 @@
 module swingx.core {
 	exports org.jdesktop.swingx;
+	
+	// Definiert Schnittstelle und exportiert sie
+	uses org.jdesktop.swingx.plaf.LookAndFeelAddons;
 	exports org.jdesktop.swingx.plaf;
+	
 	exports org.jdesktop.swingx.plaf.basic;
 	exports org.jdesktop.swingx.plaf.basic.core;
 	exports org.jdesktop.swingx.plaf.metal;
 	exports org.jdesktop.swingx.plaf.nimbus;
 	exports org.jdesktop.swingx.plaf.synth;
+	
 	requires transitive java.desktop;
 	requires java.logging;
 	requires java.prefs;
